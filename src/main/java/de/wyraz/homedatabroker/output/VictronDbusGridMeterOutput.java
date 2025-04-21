e4887478417256a49de5dd1aa6b0fdf6ca2f45cc
Herman Fries <baracoder@googlemail.com>
2025-04-21 16:57:38 +0200

    Fixup

--- src/main/java/de/wyraz/homedatabroker/output/VictronDbusGridMeterOutput.java
+++ src/main/java/de/wyraz/homedatabroker/output/VictronDbusGridMeterOutput.java
@@ -60,23 +60,26 @@ public class VictronDbusGridMeterOutput extends AbstractOutput<VictronDbusGridMe
 	public static enum GridValue {
 		
 		// Energy requires unit conversion (Wh -> kWh)
-		AC_ENERGY_FORWARD("/Ac/Energy/Forward",null, STR_WATT, KILO),
-		AC_ENERGY_REVERSE("/Ac/Energy/Reverse",null, STR_WATT, KILO),
+		//AC_ENERGY_FORWARD("/Ac/Energy/Forward",null, STR_WATT, KILO),
+		//AC_ENERGY_REVERSE("/Ac/Energy/Reverse",null, STR_WATT, KILO),
+
+		AC_L1_ENERGY_FORWARD("/Ac/L1/Energy/Forward",null, STR_WATT, KILO),
+		AC_L1_ENERGY_REVERSE("/Ac/L1/Energy/Reverse",null, STR_WATT, KILO),
 		
 		
-		AC_POWER("/Ac/Power",null,STR_WATT, NO_SCALE),
+		//AC_POWER("/Ac/Power",null,STR_WATT, NO_SCALE),
 		
 		AC_L1_VOLTAGE("/Ac/L1/Voltage",null,STR_VOLT, NO_SCALE),
 		AC_L1_CURRENT("/Ac/L1/Current",null,STR_AMPERE, NO_SCALE),
 		AC_L1_POWER("/Ac/L1/Power",null,STR_WATT, NO_SCALE),
 		
-		AC_L2_VOLTAGE("/Ac/L2/Voltage",null,STR_VOLT, NO_SCALE),
-		AC_L2_CURRENT("/Ac/L2/Current",null,STR_AMPERE, NO_SCALE),
-		AC_L2_POWER("/Ac/L2/Power",null,STR_WATT, NO_SCALE),
+		//AC_L2_VOLTAGE("/Ac/L2/Voltage",null,STR_VOLT, NO_SCALE),
+		//AC_L2_CURRENT("/Ac/L2/Current",null,STR_AMPERE, NO_SCALE),
+		//AC_L2_POWER("/Ac/L2/Power",null,STR_WATT, NO_SCALE),
 
-		AC_L3_VOLTAGE("/Ac/L3/Voltage",null,STR_VOLT, NO_SCALE),
-		AC_L3_CURRENT("/Ac/L3/Current",null,STR_AMPERE, NO_SCALE),
-		AC_L3_POWER("/Ac/L3/Power",null,STR_WATT, NO_SCALE),
+		//AC_L3_VOLTAGE("/Ac/L3/Voltage",null,STR_VOLT, NO_SCALE),
+		//AC_L3_CURRENT("/Ac/L3/Current",null,STR_AMPERE, NO_SCALE),
+		//AC_L3_POWER("/Ac/L3/Power",null,STR_WATT, NO_SCALE),
 		;
 		
 		protected final String path;
